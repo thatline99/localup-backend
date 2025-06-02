a9f2c356f49c05922ab22176331b6686e9061b7c
mepwj <mepwj@dataworld.co.kr>
2025-06-02 11:21:38 +0900

    init: localup backend 세팅

---
+++ src/main/kotlin/thatline/localup/LocalupApplication.kt
@@ -0,0 +1,11 @@
+package thatline.localup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LocalupApplication
+
+fun main(args: Array<String>) {
+	runApplication<LocalupApplication>(*args)
+}
---
+++ src/test/kotlin/thatline/localup/LocalupApplicationTests.kt
@@ -0,0 +1,13 @@
+package thatline.localup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
