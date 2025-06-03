a37fd378a038f5b115eba56386a3c32746669cf6
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-03 15:54:21 +0900

    feat: BaseException 추가 및 설정

--- src/main/kotlin/thatline/localup/exception/DuplicateEmailException.kt
+++ src/main/kotlin/thatline/localup/exception/DuplicateEmailException.kt
@@ -1,4 +1,3 @@
 package thatline.localup.exception
 
-// TODO: noah, base 추가, exception 정의
-class DuplicateEmailException : RuntimeException()
+class DuplicateEmailException : BaseException("DUPLICATE_EMAIL")
