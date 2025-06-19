6104a1e6b3d721071cfb84c1ebef5afe881cc501
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-19 21:54:28 +0900

    feat: 기상청_단기예보 ((구)_동네예보) 조회서비스 프로퍼티 추가

---
+++ src/main/kotlin/thatline/localup/property/EtcApiProperty.kt
@@ -0,0 +1,30 @@
+package thatline.localup.property
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.validation.annotation.Validated
+
+@ConfigurationProperties(prefix = "etc-api")
+@Validated
+class EtcApiProperty(
+    val kma: Kma,
+) {
+    data class Kma(
+        val baseUrl: String,
+        val vilageFcstInfoService: VilageFcstInfoService,
+    ) {
+        data class VilageFcstInfoService(
+            val firstPath: String,
+            val serviceKey: String,
+            val getUltraSrtNcst: GetUltraSrtNcst,
+            val getFcstVersion: GetFcstVersion,
+        ) {
+            data class GetUltraSrtNcst(
+                val secondPath: String,
+            )
+
+            data class GetFcstVersion(
+                val secondPath: String,
+            )
+        }
+    }
+}
