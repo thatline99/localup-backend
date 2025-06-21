d6625be2bcf85f887e7377ca6dd2ac500698df2c
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-21 19:00:52 +0900

    docs: Tour API 응답, Kdoc 작성

--- src/main/kotlin/thatline/localup/tourapi/response/MetcoRegnVisitrDDListResponse.kt
+++ src/main/kotlin/thatline/localup/tourapi/response/MetcoRegnVisitrDDListResponse.kt
@@ -1,37 +1,81 @@
 package thatline.localup.tourapi.response
 
+/**
+ * 한국관광공사_관광빅데이터 정보서비스_GW: 관광역 지자체 지역방문자수 집계 데이터 정보 조회
+ *
+ * @property response 응답
+ *
+ * @see <a href="https://www.data.go.kr/data/15101972/openapi.do">공공데이터포털 API 문서</a>
+ */
 data class MetcoRegnVisitrDDListResponse(
-    val response: MetcoRegnVisitrDDListResponseWrapper = MetcoRegnVisitrDDListResponseWrapper(),
-)
+    val response: Response,
+) {
+    /**
+     * 응답
+     *
+     * @property header 헤더
+     * @property body 본문
+     */
+    data class Response(
+        val header: Header,
+        val body: Body,
+    )
 
-data class MetcoRegnVisitrDDListResponseWrapper(
-    val header: MetcoRegnVisitrDDListHeader = MetcoRegnVisitrDDListHeader(),
-    val body: MetcoRegnVisitrDDListBody = MetcoRegnVisitrDDListBody(),
-)
+    /**
+     * 헤더
+     *
+     * @property resultCode 결과 코드
+     * @property resultMsg 결과 메시지
+     */
+    data class Header(
+        val resultCode: String,
+        val resultMsg: String,
+    )
 
-data class MetcoRegnVisitrDDListHeader(
-    val resultCode: String = "",
-    val resultMsg: String = "",
-)
+    /**
+     * 본문
+     *
+     * @property items 목록
+     * @property numOfRows 한 페이지 결과 수
+     * @property pageNo 페이지 번호
+     * @property totalCount 전체 결과 수
+     */
+    data class Body(
+        val items: Items,
+        val numOfRows: Long,
+        val pageNo: Long,
+        val totalCount: Long,
+    )
 
-data class MetcoRegnVisitrDDListBody(
-    val items: MetcoRegnVisitrDDListItems = MetcoRegnVisitrDDListItems(),
-    val numOfRows: Long = 0,
-    val pageNo: Long = 0,
-    val totalCount: Long = 0,
-)
+    /**
+     * 목록
+     *
+     * @property item 항목들
+     */
+    data class Items(
+        val item: List<Item>,
+    )
 
-data class MetcoRegnVisitrDDListItems(
-    val item: List<MetcoRegnVisitrDDListItem> = emptyList(),
-)
-
-data class MetcoRegnVisitrDDListItem(
-    val areaCode: String = "",
-    val areaNm: String = "",
-    val daywkDivCd: String = "",
-    val daywkDivNm: String = "",
-    val touDivCd: String = "",
-    val touDivNm: String = "",
-    val touNum: String = "",
-    val baseYmd: String = "",
-)
+    /**
+     * 항목
+     *
+     * @property areaCode 시도 코드
+     * @property areaNm 시도명
+     * @property daywkDivCd 요일 구분 코드
+     * @property daywkDivNm 요일 구분명
+     * @property touDivCd 관광객 구분 코드
+     * @property touDivNm 관광객 구분명
+     * @property touNum 관광객 수
+     * @property baseYmd 기준 연월일
+     */
+    data class Item(
+        val areaCode: String,
+        val areaNm: String,
+        val daywkDivCd: String,
+        val daywkDivNm: String,
+        val touDivCd: String,
+        val touDivNm: String,
+        val touNum: String,
+        val baseYmd: String,
+    )
+}
--- src/main/kotlin/thatline/localup/tourapi/response/TatsCnctrRatedListResponse.kt
+++ src/main/kotlin/thatline/localup/tourapi/response/TatsCnctrRatedListResponse.kt
@@ -1,36 +1,79 @@
 package thatline.localup.tourapi.response
 
+/**
+ * 한국관광공사_관광지 집중률 방문자 추이 예측 정보: 관광지 집중률 정보 목록조회
+ *
+ * @property response 응답
+ *
+ * @see <a href="https://www.data.go.kr/data/15128555/openapi.do">공공데이터포털 API 문서</a>
+ */
 data class TatsCnctrRatedListResponse(
-    val response: TatsCnctrRatedListResponseWrapper = TatsCnctrRatedListResponseWrapper(),
-)
+    val response: Response,
+) {
+    /**
+     * 응답
+     *
+     * @property header 헤더
+     * @property body 본문
+     */
+    data class Response(
+        val header: Header,
+        val body: Body,
+    )
 
-data class TatsCnctrRatedListResponseWrapper(
-    val header: TatsCnctrRatedListHeader = TatsCnctrRatedListHeader(),
-    val body: TatsCnctrRatedListBody = TatsCnctrRatedListBody(),
-)
+    /**
+     * 헤더
+     *
+     * @property resultCode 결과 코드
+     * @property resultMsg 결과 메시지
+     */
+    data class Header(
+        val resultCode: String,
+        val resultMsg: String,
+    )
 
-data class TatsCnctrRatedListHeader(
-    val resultCode: String = "",
-    val resultMsg: String = "",
-)
+    /**
+     * 본문
+     *
+     * @property items 목록
+     * @property numOfRows 한 페이지 결과 수
+     * @property pageNo 페이지 번호
+     * @property totalCount 전체 결과 수
+     */
+    data class Body(
+        val items: Items,
+        val numOfRows: Long,
+        val pageNo: Long,
+        val totalCount: Long,
+    )
 
-data class TatsCnctrRatedListBody(
-    val items: TatsCnctrRatedListItems = TatsCnctrRatedListItems(),
-    val numOfRows: Long = 0,
-    val pageNo: Long = 0,
-    val totalCount: Long = 0,
-)
+    /**
+     * 목록
+     *
+     * @property item 항목들
+     */
+    data class Items(
+        val item: List<Item>,
+    )
 
-data class TatsCnctrRatedListItems(
-    val item: List<TatsCnctrRatedListItem> = emptyList(),
-)
-
-data class TatsCnctrRatedListItem(
-    val baseYmd: String = "",
-    val areaCd: String = "",
-    val areaNm: String = "",
-    val signguCd: String = "",
-    val signguNm: String = "",
-    val tAtsNm: String = "",
-    val cnctrRate: Double = 0.0,
-)
+    /**
+     * 항목
+     *
+     * @property baseYmd 기준 연월일
+     * @property areaCd 지역 코드
+     * @property areaNm 지역명
+     * @property signguCd 시군구 코드
+     * @property signguNm 시군구명
+     * @property tAtsNm 관광지명
+     * @property cnctrRate 집중률
+     */
+    data class Item(
+        val baseYmd: String,
+        val areaCd: String,
+        val areaNm: String,
+        val signguCd: String,
+        val signguNm: String,
+        val tAtsNm: String,
+        val cnctrRate: Double,
+    )
+}
