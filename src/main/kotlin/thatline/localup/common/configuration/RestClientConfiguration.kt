9cfffb32b1f44aa1ee079aa2987f14d0d86f3539
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-21 17:31:37 +0900

    fix: 네이밍 통일

--- src/main/kotlin/thatline/localup/common/configuration/RestClientConfiguration.kt
+++ src/main/kotlin/thatline/localup/common/configuration/RestClientConfiguration.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 
 @Configuration
-class RestClientConfig {
+class RestClientConfiguration {
     @Bean
     fun restClient(): RestClient {
         return RestClient.create()
