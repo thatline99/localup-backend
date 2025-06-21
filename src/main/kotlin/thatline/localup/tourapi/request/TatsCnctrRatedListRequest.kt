405db9aa605597675e2c1c056b11cb0809dd81b6
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-21 18:38:08 +0900

    docs: Tour API, 요청 Kdoc 작성

--- src/main/kotlin/thatline/localup/tourapi/request/TatsCnctrRatedListRequest.kt
+++ src/main/kotlin/thatline/localup/tourapi/request/TatsCnctrRatedListRequest.kt
@@ -1,5 +1,16 @@
 package thatline.localup.tourapi.request
 
+/**
+ * 한국관광공사_관광지 집중률 방문자 추이 예측 정보: 관광지 집중률 정보 목록조회
+ *
+ * @property pageNo 페이지 번호
+ * @property numOfRows 한 페이지 결과 수
+ * @property areaCd 관광지 지역 코드
+ * @property signguCd 관광지 시군구 코드
+ * @property tAtsNm 관광지명
+ *
+ * @see <a href="https://www.data.go.kr/data/15128555/openapi.do">공공데이터포털 API 문서</a>
+ */
 data class TatsCnctrRatedListRequest(
     val pageNo: Long,
     val numOfRows: Long,
