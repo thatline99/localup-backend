0082dff7921328fa06e03f55fe1015e41d079b10
49EHyeon42 <49ehyeon42@gmail.com>
2025-06-28 19:51:50 +0900

    docs: Kdoc 수정

--- src/main/kotlin/thatline/localup/tourapi/request/LdongCode2Request.kt
+++ src/main/kotlin/thatline/localup/tourapi/request/LdongCode2Request.kt
@@ -5,7 +5,7 @@ package thatline.localup.tourapi.request
  *
  * @property pageNo 페이지 번호
  * @property numOfRows 한 페이지 결과 수
- * @property lDongRegnCd 법정동 시군구 코드 (선택)
+ * @property lDongRegnCd 법정동 시도 코드 (선택)
  *
  * @see <a href="https://www.data.go.kr/data/15101578/openapi.do">공공데이터포털 API 문서</a>
  */
