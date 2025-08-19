8c185738914e378ef7b59683a23fc15acc7c0c5b
49EHyeon42 <49ehyeon42@gmail.com>
2025-08-19 15:49:37 +0900

    fix: copy 사용으로 변경

--- src/main/kotlin/thatline/localup/user/service/UserService.kt
+++ src/main/kotlin/thatline/localup/user/service/UserService.kt
@@ -119,9 +119,7 @@ class UserService(
         val foundBusiness = businessRepository.findById(businessId)
             .orElseThrow { BusinessNotRegisteredException() }
 
-        val updatedBusiness = BusinessMongoDbEntity(
-            id = businessId,
-            createdDate = foundBusiness.createdDate,
+        val updatedBusiness = foundBusiness.copy(
             lastModifiedDate = LocalDateTime.now(),
             name = businessName,
             sigunguCode = businessSigunguCode,
