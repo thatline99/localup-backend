bbd2ad88d24871490eacd3e576e2e955b10b77d7
49EHyeon42 <49ehyeon42@gmail.com>
2025-08-21 18:42:20 +0900

    style: 이름 통일

--- src/main/kotlin/thatline/localup/common/configuration/CacheConfiguration.kt
+++ src/main/kotlin/thatline/localup/common/configuration/CacheConfiguration.kt
@@ -36,8 +36,6 @@ class CacheConfiguration(
             .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
             .disableCachingNullValues()
 
-        // TODO: 네이밍 통일 필요
-
         // 대시보드, 지난 달 관광지 랭킹 정보 캐시 설정
         val lastMonthlyTouristAttractionRankingInformationCacheConfiguration = defaultCacheConfiguration
             .serializeValuesWith(
@@ -50,7 +48,7 @@ class CacheConfiguration(
             )
             .entryTtl(Duration.ofDays(31))
 
-        // 대시보드, 작년, 같은 주, 지역 방문자 수 통계 정보 캐시 설정
+        // 대시보드, 작년 같은 주, 지역 방문자 수 통계 정보 캐시 설정
         val lastYearSameWeekVisitorStatisticsInformationCacheConfiguration = defaultCacheConfiguration
             .serializeValuesWith(
                 RedisSerializationContext.SerializationPair.fromSerializer(
@@ -74,7 +72,7 @@ class CacheConfiguration(
             )
             .entryTtl(Duration.ofDays(1))
 
-        // 대시보드, 법정동 시군구 기준, 오늘 날짜 ~ 월 마지막 일 축제/공연/행사 정보 캐시 설정
+        // 대시보드, 시군구 오늘 날짜 ~ 월 마지막 일 축제/공연/행사 정보 캐시 설정
         val ongoingOrUpComingSigunguEventsFromTodayToMonthEndInformationCacheConfiguration = defaultCacheConfiguration
             .serializeValuesWith(
                 RedisSerializationContext.SerializationPair.fromSerializer(
@@ -86,7 +84,7 @@ class CacheConfiguration(
             )
             .entryTtl(Duration.ofDays(1))
 
-        // 대시보드, 날씨 정보 캐시 설정
+        // 대시보드, 시군구 날씨 정보 캐시 설정
         val weatherInformationCacheConfiguration = defaultCacheConfiguration
             .serializeValuesWith(
                 RedisSerializationContext.SerializationPair.fromSerializer(
@@ -141,24 +139,24 @@ class CacheConfiguration(
     @Bean
     fun sigunguMainEventKeyGenerator(): KeyGenerator {
         return KeyGenerator { _, _, params ->
-            val legalDongSigunguCode = params[0] as String
+            val sigunguCode = params[0] as String
             val latitude = params[1] as Double
             val longitude = params[2] as Double
 
             val savedDateTime = LocalDateTime.now().format(DateTimeUtil.DATETIME_FORMATTER_yyyyMMdd)
 
-            "$legalDongSigunguCode-$latitude-$longitude-$savedDateTime"
+            "$sigunguCode-$latitude-$longitude-$savedDateTime"
         }
     }
 
     @Bean
     fun ongoingOrUpComingSigunguEventsFromTodayToMonthEndKeyGenerator(): KeyGenerator {
         return KeyGenerator { _, _, params ->
-            val legalDongSigunguCode = params[0] as String
+            val sigunguCode = params[0] as String
 
             val savedDateTime = LocalDateTime.now().format(DateTimeUtil.DATETIME_FORMATTER_yyyyMMdd)
 
-            "$legalDongSigunguCode-$savedDateTime"
+            "$sigunguCode-$savedDateTime"
         }
     }
 
