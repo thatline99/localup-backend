78dac233a4b2a3cbab4d235656046474be70cd99
mepwj <hoholeo382@gmail.com>
2025-08-22 09:17:57 +0900

    test: 클로드 리뷰어 테스트

--- src/main/kotlin/thatline/localup/LocalupApplication.kt
+++ src/main/kotlin/thatline/localup/LocalupApplication.kt
@@ -10,4 +10,7 @@ class LocalupApplication
 
 fun main(args: Array<String>) {
     runApplication<LocalupApplication>(*args)
+
+    // 클로드 sonnet 모델을 이용한 테스트 PR 용 입니다.
+    println("Localup Application is running!")
 }
